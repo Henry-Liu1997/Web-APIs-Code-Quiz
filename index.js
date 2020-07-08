bafde1379865b30552ba30b3167d850801e682bd
Henry Liu <hl3184@columbia.edu>
2020-07-08 17:12:22 -0400

    added question render function and result check function

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// loading page
+// DOM Element
 var heading = document.querySelector('.heading');
 var start_btn = document.querySelector('.btn-start');
 var clock = document.querySelector('.time');
@@ -19,30 +19,51 @@ var questions = [
     slections: ['yes I do', 'no i do not'],
     answer: 'yes I do',
   },
+  {
+    title: 'Do you like apple',
+    slections: ['yes', 'no '],
+    answer: 'yes',
+  },
 ];
 
+// track question
+var index = 0;
+
+//create a function for render a single question
+var renderQuestion = (i) => {
+  //clear previous question
+  question__selections.textContent = '';
+
+  //render the current question
+  question__title.textContent = questions[i].title;
+  questions[i].slections.forEach((selection) => {
+    var li = document.createElement('li');
+    li.classList.add('list-group-item');
+    li.innerHTML = `<button class="btn btn-primary text-capitalize select__btn" data-id="0">${selection}</button>`;
+    question__selections.append(li);
+  });
+
+  //increase index by 1 for next render
+  index++;
+};
+
+//game start
 start_btn.addEventListener('click', () => {
   var game = setInterval(() => {
     clock.textContent = `Time: ${time}`;
     time--;
-
     if (time < 0) {
       clearInterval(game);
     }
   }, 1000);
 
   heading.style.display = 'none';
-  question__title.textContent = questions[0].title;
   start_btn.style.display = 'none';
 
-  questions[0].slections.forEach((selection) => {
-    var li = document.createElement('li');
-    li.classList.add('list-group-item');
-    li.innerHTML = `<button class="btn btn-primary text-capitalize select__btn" data-id="0">${selection}</button>`;
-    question__selections.append(li);
-  });
+  renderQuestion(0);
 });
 
+//check user's result
 question__selections.addEventListener('click', (e) => {
   if (e.target.matches('button')) {
     if (
@@ -57,4 +78,6 @@ question__selections.addEventListener('click', (e) => {
       user__result.textContent = 'Wrong....';
     }
   }
+
+  renderQuestion(index);
 });
